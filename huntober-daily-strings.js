272454aa69686a143dd4246d01b0ce891b7c3a58
Daniel Hyres <danhyres@gmail.com>
2022-10-05 18:12:34 -0400

    unfinished Wednesday sol

--- huntober-daily-strings.js
+++ huntober-daily-strings.js
@@ -131,3 +131,67 @@ function strRev(str){
 }
 
 //Day 5, Wednesday 10-5-22
+// There are some very strict rules that the pun submissions had to follow. I need to know which of these cat puns made the cut. I promise tomorrow we'll get back to decoding our secret message, and coincidentally, I'm pretty sure the number of these puns that met the criteria will match a key sequence value we'll need tomorrow. Isn't that lucky?
+
+// Here is a comma-separated list of the submissions:
+
+// "The Pawshank Redemption,Caturday Night Live,Only Meworders in the Building,I Love Mewcy, Mewsummer Meowders,The Golden Purrs, Purrlandia ,Meowpardy, Meowstery Science Theater: Purrthousand, Amewican Idol,Dog City,Doctor Mew , The Meowing Fed,Mew Peter,The Vicar of Dogley, Kittens,Meownton Abbey,Pets and the Kitty,Dogis and Bonehead,Pawlty Meowers ,The Meowpet Show,Barkos,The Catbert Purrport,The Pawffice,The Dogford Files, Battlestar Catlactica,Catlumbo,SpongeDog Squarepants,NYPD Mew ,Fluffy the Meowpire Purrer,The Inbemewners,Meowder She Wrote,Paw & Order,30 Dog, Pawvatar: The Last Meowbender,The Pawnight Show,Arrested Dogvelopment,Furiends,Mewie,Curb Your Dogthusiasm,Teenage Mewtant Ninja Turtles,Phineas and Purrb,Paw Trek, Paw Trek: The Next Mewination, Twin Mewks, *C*A*T*S*,DogTales, Game of Bones, House of the Meowgon,The Purrlight Zone,Breaking Bone,The Meowre,The Dogpranos,The Rings of Meower, The KIT Crowd,Strangepaw Things ,Catman: The Animeowted Series,Meowter Call Saul,Mewgerton ,Obark,Mewphoria,La Casa de Pawpel,Rick & Meowty,Amewican Purror Story, Mewcifer,PawndaVision,Dogxter,The Meowndalorian, Dog Lasso,Bark,Meowdern Pawmily , Meowtlander,Bone Mirror,Barks and Recreation,How to Get Away with Meowder,Boneland ,Meowther Ted,Mewtopia,Mewey,The Mewkie Meowse Doghouse,Mewster Rogers' Neighborhood"
+
+// -No empty spaces at the start or end of the submission (my friend can't stand when people don't follow directions)
+// -Cannot contain 'dog', 'bark', or 'bone' in any combination of upper or lower case (canine trolling can't be stopped)
+// -Total length of pun cannot be a multiple of 5 (this includes spaces and punctuation)
+// -The sum of the charCodes of the 1st and last characters must be odd (my friend's a bit of a character, but she's great once you get to know her)
+// -Character directly after the middle of the string may not be 'e' (For example the character to check in even-length string center is t, for odd an example would be weird -> 'r')
+// -Must have an even number of lowercase letters (do not count punctuation or spaces as letters)
+// -Must have at least 2 capital letters (honestly, I'm a little worried these requirements are too strict)
+// -Must not contain a capital 'S' (definitely too strict. What does S even have to do with cat puns?)
+
+//PREP:
+//P: String 
+//R: Strings?
+//E:
+/*
+P: function punSubs (arr){
+  thinking, put all string submissions into array, then loop through array and check for each rule?
+}
+*/
+
+function punSubs (subStr){
+
+  let subArr = subStr.split(',')
+  console.log(subArr)
+
+  let spaceFilt = subArr.filter((x,i) => x[0] !== ' ' )
+  console.log(spaceFilt)
+
+  let dogBarkBoneFilt = spaceFilt.filter((x,i) => !x.toLowerCase().includes('dog') ).filter((x,i) => !x.toLowerCase().includes('bark') ).filter((x,i) => !x.toLowerCase().includes('bone'))
+  console.log(dogBarkBoneFilt)
+
+  let totalLengthFilt = dogBarkBoneFilt.filter((x)=> x.length % 5 !== 0)
+  console.log(totalLengthFilt) //37 left
+
+  let sumCharCodesFilt = totalLengthFilt.filter((x,i) => {
+    return (x.charCodeAt(0) + x.charCodeAt(x.length-1)) % 2 !== 0
+  })
+  console.log(sumCharCodesFilt) //16 left
+
+
+  let middleCharFilt = sumCharCodesFilt.filter( (x,i) => {
+    return ( x.length % 2 === 0 ? x.charAt((x.length)/2 + 1) !== 'e' : x.charAt( Math.ceil(x.length/2)) !== 'e' )
+  })
+  console.log(middleCharFilt) //14 remain
+
+  //check must have even number of lowercase letters
+  let lowerCaseCount = middleCharFilt.filter( (x) => {
+    x.split(',')
+  })
+
+  //check must have at least 2 capital letters
+
+  //check must not contain capital 'S'
+
+}
+
+
+
+// "The Pawshank Redemption,Caturday Night Live,Only Meworders in the Building,I Love Mewcy, Mewsummer Meowders,The Golden Purrs, Purrlandia ,Meowpardy, Meowstery Science Theater: Purrthousand, Amewican Idol,Dog City,Doctor Mew , The Meowing Fed,Mew Peter,The Vicar of Dogley, Kittens,Meownton Abbey,Pets and the Kitty,Dogis and Bonehead,Pawlty Meowers ,The Meowpet Show,Barkos,The Catbert Purrport,The Pawffice,The Dogford Files, Battlestar Catlactica,Catlumbo,SpongeDog Squarepants,NYPD Mew ,Fluffy the Meowpire Purrer,The Inbemewners,Meowder She Wrote,Paw & Order,30 Dog, Pawvatar: The Last Meowbender,The Pawnight Show,Arrested Dogvelopment,Furiends,Mewie,Curb Your Dogthusiasm,Teenage Mewtant Ninja Turtles,Phineas and Purrb,Paw Trek, Paw Trek: The Next Mewination, Twin Mewks, *C*A*T*S*,DogTales, Game of Bones, House of the Meowgon,The Purrlight Zone,Breaking Bone,The Meowre,The Dogpranos,The Rings of Meower, The KIT Crowd,Strangepaw Things ,Catman: The Animeowted Series,Meowter Call Saul,Mewgerton ,Obark,Mewphoria,La Casa de Pawpel,Rick & Meowty,Amewican Purror Story, Mewcifer,PawndaVision,Dogxter,The Meowndalorian, Dog Lasso,Bark,Meowdern Pawmily , Meowtlander,Bone Mirror,Barks and Recreation,How to Get Away with Meowder,Boneland ,Meowther Ted,Mewtopia,Mewey,The Mewkie Meowse Doghouse,Mewster Rogers' Neighborhood"
