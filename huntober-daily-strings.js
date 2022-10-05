40194bc02c41154fbdfbce2df7ebf2ef34ec3670
Daniel Hyres <danhyres@gmail.com>
2022-10-05 18:27:51 -0400

    unfinished wednesday sol

--- huntober-daily-strings.js
+++ huntober-daily-strings.js
@@ -159,26 +159,31 @@ P: function punSubs (arr){
 function punSubs (subStr){
 
   let subArr = subStr.split(',')
-  console.log(subArr)
+  console.log(subArr) //81 total
 
-  let spaceFilt = subArr.filter((x,i) => x[0] !== ' ' )
-  console.log(spaceFilt)
+  //Check no empty spaces at start or end
+  let spaceFilt = subArr.filter((x,i) => x[0] !== ' ' ).filter((x,i) => x[x.length-1] !== ' ' )
+  console.log(spaceFilt) //57 left
 
+  //Filter out submissions with 'dog', 'bark', and 'bone' (all casings)
   let dogBarkBoneFilt = spaceFilt.filter((x,i) => !x.toLowerCase().includes('dog') ).filter((x,i) => !x.toLowerCase().includes('bark') ).filter((x,i) => !x.toLowerCase().includes('bone'))
-  console.log(dogBarkBoneFilt)
+  console.log(dogBarkBoneFilt) //39
 
+  //check total length not a multiple of 5
   let totalLengthFilt = dogBarkBoneFilt.filter((x)=> x.length % 5 !== 0)
-  console.log(totalLengthFilt) //37 left
+  console.log(totalLengthFilt) //33 
 
+  //check sum of first and last char codes are ODD
   let sumCharCodesFilt = totalLengthFilt.filter((x,i) => {
     return (x.charCodeAt(0) + x.charCodeAt(x.length-1)) % 2 !== 0
   })
-  console.log(sumCharCodesFilt) //16 left
+  console.log(sumCharCodesFilt) //14 
 
+  //Check character after middle is not 'e'
   let middleCharFilt = sumCharCodesFilt.filter( (x,i) => {
     return ( x.length % 2 === 0 ? x.charAt((x.length)/2 + 1) !== 'e' : x.charAt( Math.ceil(x.length/2)) !== 'e' )
   })
-  console.log(middleCharFilt) //14 remain
+  console.log(middleCharFilt) //12 
 
   //check must have even number of lowercase letters
   let lowerCaseCount = middleCharFilt.filter( (x) => {
