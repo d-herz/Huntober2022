5295daa84d1041054bb70d2b704a28b31db34e29
Daniel Hyres <danhyres@gmail.com>
2022-10-12 18:30:46 -0400

    fix

--- huntober-daily-standup.js
+++ huntober-daily-standup.js
@@ -2,7 +2,7 @@
 
 // Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 3, print out “Fizz”; when it’s divisible by 5, print out “Buzz”; when it’s divisible by both 3 and 5, print out “Fizz Buzz”.
 
-//P: a number, always a number?, ever empty?, always positive?, never a string or any funny stuff?
+//P: a number, always a number?, ever empty?, always positive?, never a string or any funny stuff? clarify all of this
 //R: number(s) and depending on parity a string
 //E: fizzBuzz (6) === 1,2,'Fizz',4,'Buzz','Fizz'
 /*
@@ -10,7 +10,7 @@ P: loop, conditional checking parity
 */
 
 function fizzBuzz(n){
-  for(let i=0; i<= n; i++){
+  for(let i=1; i<= n; i++){
     if(i % 3 === 0 && i % 5 === 0 ){
       console.log(i + ' Fizz Buzz')
     }else if (i % 5 === 0){
