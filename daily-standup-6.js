fa3f7ebb003239601537c5682da18d7e2938a29d
Daniel Hyres <danhyres@gmail.com>
2022-10-24 18:25:33 -0400

    updated sol for day 6 post standup meeting

--- daily-standup-6.js
+++ daily-standup-6.js
@@ -6,15 +6,17 @@
 // ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
 // ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
 
-//parameters: 2 (array of numbers and an integer) (will it ever be empty, will it always contain the required amount of even numbers, )
+//parameters: 2 (array of numbers and an integer) (Will the array ever be empty? Will it always contain the required amount of even numbers (at least 'n')? Will it always contain only numbers (no strings, etc), always positive values (both, ok!)? all whole numbers?)
 //return a new array with the last n even numbers
 
-function lastEvens (arr, n) {
+// function lastEvens (arr, n) {
   
-  let arr2 = arr.filter( x => x % 2 ===0)
-  return arr2.slice(-n)
+//   let arr2 = arr.filter( x => x % 2 === 0)
+//   return arr2.slice(-n)
 
-}
+// }
+//1-liner
+const lastEvens = ( arr, n ) => arr.filter( x => x % 2 === 0 ).slice(-n)
 
 console.log(lastEvens([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
 console.log(lastEvens([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))
