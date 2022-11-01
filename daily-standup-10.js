830b7a901da516e79866ebea7e0e9ad4e6899bbb
Daniel Hyres <danhyres@gmail.com>
2022-11-01 12:45:54 -0400

    challenge #10 first solution

---
+++ daily-standup-10.js
@@ -0,0 +1,36 @@
+// Given a string, return a new string that has transformed based on the input:
+
+// Change case of every character, ie. lower case to upper case, upper case to lower case.
+// Reverse the order of words from the input.
+// Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+// For example:
+
+// "Example Input" ==> "iNPUT eXAMPLE"
+// You may assume the input only contain English alphabet and spaces.
+
+
+
+function stringTransformer(str) {
+  str = str.split(' ').join('-') //add a hyphen at each space
+
+  let arr = str.split(' ').reverse(); //split the string into an array, and reverse it
+
+  let word = arr.map( x => x.split('')); //loop through the array and split each word into an array of the letters
+
+  let newArr = []; //empty array initialized for pushing the opposite case letters into
+
+  for (let i = 0; i < word.length; i++) {
+    for (let j = 0; j < word[i].length; j++) {
+
+      if (word[i][j].toLowerCase() === word[i][j]) {
+        newArr.push(word[i][j].toUpperCase())
+      } else {
+        newArr.push(word[i][j].toLowerCase())
+      }
+    }
+  }
+  return newArr.join('').replace('-', ' ')
+}
+
+console.log(stringTransformer('Example Input'))
