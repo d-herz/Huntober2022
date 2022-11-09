058c7e8bccd544f481959987223f72649fb35d3e
Daniel Hyres <danhyres@gmail.com>
2022-11-09 18:17:11 -0500

    day 13 Leon solution

--- daily-standup-13.js
+++ daily-standup-13.js
@@ -1,4 +1,4 @@
-//11-9-22
+//11-9-22: Array.diff
 // https://www.codewars.com/kata/523f5d21c841566fde000009
 // Implement a difference function, which subtracts one list from another and returns the result.
 
@@ -17,10 +17,16 @@
 
 
 
-//My first highly-unoptimized solution
+// My first highly-unoptimized solution
 function arrayDiff(a, b) {
   
   return a.filter( x => !b.includes(x))
+}
+
+//Leon's more-optimal solution:
+function arrayDiff(a, b) {
+  let newSet = new Set(b)
+  return a.filter( n => !newSet.has(n) ) //has happens in constant time
 }
 
 console.log( arrayDiff([1,2],[1]) ) //[2]
@@ -28,3 +34,29 @@ console.log( arrayDiff([1,2,2,2,3],[2]) ) //[1,3]
 console.log( arrayDiff( [1,2,2], [1] ) ) //[2,2]
 console.log( arrayDiff([1,2,2], [2]) ) //[1]
 console.log( arrayDiff([1,2,2], [] ) ) //[1,2,2]
+
+
+//Other codewar user sol's
+
+// function arrayDiff(a, b) {
+//   b = new Set(b)
+//   return a.filter(v => !b.has(v))
+// }
+
+
+// function arrayDiff(a, b) {
+
+//   var arr = new Array(); //Not sure why you "new Array()" and not just []
+  
+//   for(var i = 0;i<a.length;i++){ //b.indexOf(a[i] < 0 ) because if not in b, indexOf returns -1
+//       if(b.indexOf(a[i])<0){
+//           arr.push(a[i]);
+//       }
+//   }
+
+//   return arr;
+// }
+
+// function arrayDiff(a, b) {
+//   return a.filter( v => { return b.indexOf(v) === -1 });
+// }
