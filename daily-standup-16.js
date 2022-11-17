cd91cdb36f4d420007473465ec475f1411521db2
Daniel Hyres <danhyres@gmail.com>
2022-11-17 18:18:11 -0500

    Updated day 16 sol with Leon's solution

--- daily-standup-16.js
+++ daily-standup-16.js
@@ -13,36 +13,48 @@
 
 //Will the array elements always consist of the same letter? Never any empty or different value types?
 
+
+//My solution
+// function averageLength(arr) {
+//   let arrLength = arr.length
+//   // console.log(arrLength)
+
+//   let totalStrLength = 0
+//   arr.forEach(ele => totalStrLength += ele.length)
+//   // console.log(totalStrLength)
+
+//   let avgElementLength = Math.round(totalStrLength / arrLength)
+//   console.log(` The average length is ${ avgElementLength }`)
+
+//   let newArr = arr.map( x => x.split(''))
+//   // console.log(newArr)
+
+//   for (let i = 0; i < newArr.length; i++) {
+//     while (newArr[i].length > avgElementLength) {
+//       newArr[i].pop();
+//     }
+
+//     while (newArr[i].length < avgElementLength) {
+//       newArr[i].push(newArr[i][0])
+//     }
+//   }
+//   newArr = newArr.map( x => x.join(''))
+
+//   return newArr
+// }
+
+
+//Leon Solution
 function averageLength(arr) {
-  let arrLength = arr.length
-  // console.log(arrLength)
 
-  let totalStrLength = 0
-  arr.forEach(ele => totalStrLength += ele.length)
-  // console.log(totalStrLength)
+  //get avg length -> rounded
+  const avgLength = Math.round( arr.join('').length /arr.length )
 
-  let avgElementLength = Math.round(totalStrLength / arrLength)
-  console.log(` The average length is ${ avgElementLength }`)
-
-  let newArr = arr.map( x => x.split(''))
-  // console.log(newArr)
-
-  for (let i = 0; i < newArr.length; i++) {
-    while (newArr[i].length > avgElementLength) {
-      newArr[i].pop();
-    }
-
-    while (newArr[i].length < avgElementLength) {
-      newArr[i].push(newArr[i][0])
-    }
-
-  }
-  newArr = newArr.map( x => x.join(''))
-
-  return newArr
+  return arr.map( str => str[0].repeat(avgLength) )
 }
 
-// console.log(averageLength(['u', 'y'])) // ['u', 'y'] avg length = 1
-// console.log(averageLength(['aa', 'bbb', 'cccc'])) // ['aaa', 'bbb', 'ccc'] avg length = 3
-// console.log(averageLength(['aa', 'bb', 'ddd', 'eee'])) // ['aaa', 'bbb', 'ddd', 'eee'] avg length = 2.5;
+
+console.log(averageLength(['u', 'y'])) // ['u', 'y'] avg length = 1
+console.log(averageLength(['aa', 'bbb', 'cccc'])) // ['aaa', 'bbb', 'ccc'] avg length = 3
+console.log(averageLength(['aa', 'bb', 'ddd', 'eee'])) // ['aaa', 'bbb', 'ddd', 'eee'] avg length = 2.5;
 console.log(averageLength(['vvv', 'i', 'iiiiiiiiii', 'kkkkkkkkk', 'g', 'qqqqqq'])) //avg length = 5
