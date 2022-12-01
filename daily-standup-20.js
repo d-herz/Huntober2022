a4391d7f50001635d8b53245709ada7825b1b9e1
Daniel Hyres <danhyres@gmail.com>
2022-12-01 18:23:54 -0500

    Updated day 20 sol

--- daily-standup-20.js
+++ daily-standup-20.js
@@ -1,6 +1,8 @@
 //12-1-22
 // Given a number as an input, print out every integer from 1 to that number.However, when the integer is divisible by 2, print out “Fizz”; when it’s divisible by 3, print out “Buzz”; when it’s divisible by both 2 and 3, print out “Fizz Buzz”.
 
+//Will it always be a whole number, a positive number? 0 (no) ? Anything weird or no?
+
 function fizzBuzz(n) {
   for (let i = 1; i <= n; i++) {
     if (i % 2 === 0 && i % 3 === 0) {
@@ -11,6 +13,24 @@ function fizzBuzz(n) {
       
     } else if (i % 2 === 0) {
       console.log( 'Fizz')
+    } else {
+      console.log(i)
+    }
+  }
+}
+
+//Leon's solution //only difference was && for first condition
+
+function fizzBuzzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 6 === 0 ) { 
+    console.log('Fizzbuzz')
+
+    } else if (i % 3 === 0) {
+      console.log('Buzz')
+
+    } else if (i % 2 === 0) {
+      console.log('Fizz')
     } else {
       console.log(i)
     }
