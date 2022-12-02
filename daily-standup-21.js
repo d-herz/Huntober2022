7e76e92197526e9e91f9a4cb6880ef1540231fb0
Daniel Hyres <danhyres@gmail.com>
2022-12-02 18:18:12 -0500

    Updated day 21 w/Leon's solution

--- daily-standup-21.js
+++ daily-standup-21.js
@@ -3,20 +3,35 @@
 
 // Given a string of characters as input, write a function that returns it with the characters reversed.No Reverse Method(well brute force it first, but then no reverse method)! 
 
-//always a string of characters? No empty inputs, integers, or other weird stuff?
+//always a string of characters? No empty inputs, integers, or other weird stuff like special characters, etc?
+//Does casing matter? No, ok...
 //Return the same string with characters in the reverse order (without reverse method)
 
 //My sol
-function revString(str) { 
-  // console.log(str.length) //sanity check
-  //initialize new string
-  let newStr = '' 
+// function revString(str) { 
+//   // console.log(str.length) //sanity check
+//   //initialize new string
+//   let newStr = '' 
   
-  //loop through original string backwards and add each letter (starting from the end) to the new string
-  for (let i = str.length-1; i >= 0; i--) {
-    newStr += str[i]
+//   //loop through original string backwards and add each letter (starting from the end) to the new string
+//   for (let i = str.length-1; i >= 0; i--) {
+//     newStr += str[i]
+//   }
+//   return newStr
+// }
+
+//Leon's Sol
+
+function revString(str) {
+  //brute force first
+  // return str.split('').reverse().join('')
+
+  let reverseStr = ''
+  for (let char of str) {
+    reverseStr = char + reverseStr
   }
-  return newStr
+  return reverseStr
+  
 }
 
 
