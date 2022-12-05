2843903ffe5701b9d5b65b73f15f3847458c4aac
Daniel Hyres <danhyres@gmail.com>
2022-12-05 18:16:03 -0500

    Updated day 22 w/Leon

--- daily-standup-22.js
+++ daily-standup-22.js
@@ -3,27 +3,45 @@
 // Count all the occurring characters in a string. If you have a string like aba, then the result should be { 'a': 2, 'b': 1 }.
 // What if the string is empty ? Then the result should be empty object literal, {}.
 
+//prep as usual, parameters, return, etc.
+//My sol
+// function count(str) {
+//   let cache = {}
+//   if (str) {
+//     let arrStr = str.split('')
 
+//     for (let i = 0; i < arrStr.length; i++) {
+//       if (cache.hasOwnProperty(arrStr[i])) {
+//         cache[arrStr[i]]++
+
+//       } else {
+//         cache[arrStr[i]] = 1
+
+//       }
+//     }
+//   } else {
+//     return cache
+//   }
+//   return cache;
+// }
+
+//Leon's solution
 function count(str) {
-  let cache = {}
-  if (str) {
-    let arrStr = str.split('')
+  //create object
+  let countObject = {}
+  //loop -> check val exists ? val ++ : val = 1
 
-    for (let i = 0; i < arrStr.length; i++) {
-      if (cache.hasOwnProperty(arrStr[i])) {
-        cache[arrStr[i]]++
-
-      } else {
-        cache[arrStr[i]] = 1
-
-      }
+  for (const char of str) { //for of loop
+    if ( countObject[char] ) { //Don't need .hasOwnProperty, just check using square bracket notation
+      countObject[char]++
+    } else {
+      countObject[char] = 1
     }
-  } else {
-    return cache
   }
-  return cache;
-}
+  
+  return countObject
 
+}
 
 console.log(count('aba')) // { 'a': 2, 'b': 1 }
 console.log(count('')) //{}
