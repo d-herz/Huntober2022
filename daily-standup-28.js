19bfc8bde7abdbe2ac9afbf315c66b92d9ff58e9
Daniel Hyres <danhyres@gmail.com>
2022-12-15 18:21:25 -0500

    Day 28 challenge and solution

--- daily-standup-28.js
+++ daily-standup-28.js
@@ -69,8 +69,6 @@ function twoSum2(arr, sum) {
   return pairs
 }
 
-
-
 // console.log(twoSum([2, 7, 11, 15], 9)) // [0,1]
 // console.log(twoSum([3, 2, 4], 6)) // [1,2]
 // console.log(twoSum([3, 3], 6)) // [0,1]
