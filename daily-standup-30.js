a3970cc6807434c59e93e8763c30471a745bff90
Daniel Hyres <danhyres@gmail.com>
2023-01-03 17:01:34 -0500

    Standup 30 Challenge

---
+++ daily-standup-30.js
@@ -0,0 +1,44 @@
+//01-03-2023
+//(Same as Standup 29: can you solve with a for loop like previous? How about with recursion?)
+
+//https://leetcode.com/problems/fibonacci-number/
+
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+//   F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// Given n, calculate F(n).
+
+//     Example 1:
+// Input: n = 2
+// Output: 1
+// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+// Example 2:
+// Input: n = 3
+// Output: 2
+// Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+// Example 3:
+// Input: n = 4
+// Output: 3
+// Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3
+
+//For loop
+var fib = function (n) {
+ 
+};
+
+//Recursion
+var fib = function (n) {
+
+};
+
+
+console.log(fib(0)) // 0
+console.log(fib(1)) // 1
+console.log(fib(2)) // 1
+console.log(fib(3)) // 2
+console.log(fib(4)) // 3
+console.log(fib(5)) // 5
+console.log(fib(10)) // 55 
