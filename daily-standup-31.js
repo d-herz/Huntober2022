4cd054edd208e67b5cd6be07f7739bf8513470da
Daniel Hyres <danhyres@gmail.com>
2023-01-18 17:45:17 -0500

    Recursive Factorial Function

---
+++ daily-standup-31.js
@@ -0,0 +1,31 @@
+//1-18-23
+//https://www.codewars.com/kata/5694cb0ec554589633000036
+
+// In mathematics, the factorial of a non - negative integer n, denoted by n!, is the product of all positive integers less than or equal to n.For example,
+
+// 5! = 5 * 4 * 3 * 2 * 1 = 120.
+
+// The value of 0! is 1.
+
+// #Your task
+
+// You have to create the function factorial that receives n and returns n! You have to use recursion.
+
+
+
+
+
+function factorial(n) {
+
+  if (n === 0) {
+    return 1
+  } else {
+    return n * factorial(n-1)
+  }
+
+}
+
+
+console.log(factorial(3)) // 6
+console.log(factorial(5)) // 120
+console.log(factorial(10)) // 3,628,800
