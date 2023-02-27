8f9524c1af1f7286a3ac03308364962455591166
Daniel Hyres <danhyres@gmail.com>
2023-02-27 17:57:48 -0500

    SU-34 Middle of the Linked List

---
+++ daily-standup-34.js
@@ -0,0 +1,28 @@
+// Standup 02-27-2023
+// Leetcode: Middle of the Linked List
+// https://leetcode.com/problems/middle-of-the-linked-list/
+
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+
+};
+
+console.log( middleNode() )
+console.log( middleNode() )
+console.log( middleNode() )
+console.log( middleNode() )
