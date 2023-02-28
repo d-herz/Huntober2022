56eabcc714af3cd9e1dd1eb5c59a3e5dbf601cfb
Daniel Hyres <danhyres@gmail.com>
2023-02-28 18:35:34 -0500

    SU-35 singly linked list with reverse method

--- daily-standup-35.js
+++ daily-standup-35.js
@@ -17,7 +17,52 @@
  * @return {ListNode}
  */
 
+class LinkedList {
+  constructor() {
+    this.head = this.tail = null
+  }
+
+  append(value) {
+
+    if (!this.tail) {
+      this.head = this.tail = new Node(value)
+    }
+
+    else {
+      let oldTail = this.tail
+      this.tail = new Node(value)
+      oldTail.next = this.tail
+    }
+  }
+  reverseList() {
+    let current = this.head
+    let prev = null
+    while (current) {
+      const temp = current.next
+      current.next = prev
+      prev = current
+      current = temp
+    }
+    return prev
+  }
+}
+
+class Node {
+  constructor(value, prev, next) {
+    this.value = value
+    this.next = next || null
+  }
+}
+
+const list = new LinkedList()
+
+list.append(1)
+list.append(2)
+list.append(3) 
 
 var reverseList = function (head) {
 
+  
 };
+
+
