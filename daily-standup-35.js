ffdb0b52275f986ae725fdf7f0dfd8c5015d9922
Daniel Hyres <danhyres@gmail.com>
2023-02-28 17:27:14 -0500

    SU-35 challenge

--- daily-standup-35.js
+++ daily-standup-35.js
@@ -1,3 +1,23 @@
 // Standup 2-28-2023
 // Leetcode: Reverse Linked List
 // https://leetcode.com/problems/reverse-linked-list/
+
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+
+var reverseList = function (head) {
+
+};
