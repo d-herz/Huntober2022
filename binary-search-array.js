2ad819f8de99c651e8d88a423dd963a830252815
Daniel Hyres <danhyres@gmail.com>
2023-03-17 18:17:31 -0400

    Updates

--- binary-search-array.js
+++ binary-search-array.js
@@ -2,15 +2,21 @@
 // Solves Leetcode: https://leetcode.com/problems/binary-search/description/
 
 function binarySearch(nums, target) {
-  let left = 0, right = nums.length - 1
+  let left = 0;
+  let right = nums.length - 1;
+
   while (left < right) {
-    let mid = left + Math.floor((right - left + 1) / 2)
+    let mid = Math.ceil((left + right) / 2)
+    // let mid = left + Math.floor((right - left + 1) / 2)
+    
     if (target < nums[mid]) {
       right = mid - 1
     } else {
-      left = mid
+      left = mid + 1
+      // left = mid
     }
   }
   return nums[left] === target ? left : -1
 }
 console.log(binarySearch([-1, 0, 3, 5, 9, 12], 9), "4")
+console.log(binarySearch([-1, 0, 3, 5, 9, 12], 9), "-1")
--- daily-standup-37.js
+++ daily-standup-37.js
@@ -2,7 +2,7 @@
 // Leetcode: Binary Search
 // https://leetcode.com/problems/binary-search/description/
 
-// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.If target exists, then return its index.Otherwise, return -1.
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its INDEX. Otherwise, return -1.
 
 // You must write an algorithm with O(log n) runtime complexity.
 
